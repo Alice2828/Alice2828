5746246efeacf1a0ceaa1756b654a2093ae46fdf
Alice2828 <47051363+Alice2828@users.noreply.github.com>
2020-04-22 18:42:45 +0600

    Broadcast no intent

--- app/src/main/java/com/example/movie/service/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/movie/service/MyFirebaseMessagingService.kt
@@ -6,10 +6,12 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
 import android.media.RingtoneManager
 import android.util.Log
 import android.widget.Toast
 import androidx.core.app.NotificationCompat
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.example.movie.BuildConfig
 import com.example.movie.R
 import com.example.movie.api.RetrofitService
@@ -23,7 +25,6 @@ import kotlin.coroutines.CoroutineContext
 
 @SuppressLint("MissingFirebaseInstanceTokenRefresh")
 class MyFirebaseMessagingService : FirebaseMessagingService(), CoroutineScope {
-    private var movie: Movie? = Singleton.getMovie()
     private val job = Job()
     override val coroutineContext: CoroutineContext
         get() = Dispatchers.Main + job
@@ -34,41 +35,47 @@ class MyFirebaseMessagingService : FirebaseMessagingService(), CoroutineScope {
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         if (remoteMessage.notification != null) {
-            showNotification(
+            handleNotification(
                 remoteMessage.notification?.title,
                 remoteMessage.notification?.body
             )
         }
     }
 
-    private fun showNotification(title: String?, body: String?) {
-        val intent = Intent(this, DetailActivity::class.java)
-        intent.putExtra("movie_id", movie?.id)
-        intent.putExtra("original_title", movie?.original_title)
-        intent.putExtra("movie", movie)
-        intent.putExtra("poster_path", movie?.getPosterPath())
-        intent.putExtra("overview", movie?.overview)
-        intent.putExtra("vote_average", (movie?.vote_average).toString())
-        intent.putExtra("release_date", movie?.release_date)
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-        val pendingIntent = PendingIntent.getActivity(
-            this, 0, intent,
-            PendingIntent.FLAG_ONE_SHOT
-        )
-
-        val soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
-        val notificationBuilder = NotificationCompat.Builder(this)
-            .setSmallIcon(R.mipmap.ic_launcher)
-            .setWhen(System.currentTimeMillis())
-            .setContentTitle(title)
-            .setContentText(body)
-            .setAutoCancel(true)
-            .setSound(soundUri)
-            .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
-            .setContentIntent(pendingIntent)
-
-        val notificationManager =
-            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        notificationManager.notify(0, notificationBuilder.build())
+    private fun handleNotification(title: String?, body: String?) {
+        val pushNotification = Intent("push")
+        pushNotification.putExtra("title", title)
+        pushNotification.putExtra("message", body)
+        LocalBroadcastManager.getInstance(this).sendBroadcast(pushNotification)
     }
+//    private fun showNotification(title: String?, body: String?) {
+//        val intent = Intent(this, DetailActivity::class.java)
+//        intent.putExtra("movie_id", movie?.id)
+//        intent.putExtra("original_title", movie?.original_title)
+//        intent.putExtra("movie", movie)
+//        intent.putExtra("poster_path", movie?.getPosterPath())
+//        intent.putExtra("overview", movie?.overview)
+//        intent.putExtra("vote_average", (movie?.vote_average).toString())
+//        intent.putExtra("release_date", movie?.release_date)
+//        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+//        val pendingIntent = PendingIntent.getActivity(
+//            this, 0, intent,
+//            PendingIntent.FLAG_ONE_SHOT
+//        )
+//
+//        val soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+//        val notificationBuilder = NotificationCompat.Builder(this)
+//            .setSmallIcon(R.mipmap.ic_launcher)
+//            .setWhen(System.currentTimeMillis())
+//            .setContentTitle(title)
+//            .setContentText(body)
+//            .setAutoCancel(true)
+//            .setSound(soundUri)
+//            .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+//            .setContentIntent(pendingIntent)
+//
+//        val notificationManager =
+//            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+//        notificationManager.notify(0, notificationBuilder.build())
+//    }
 }
--- app/src/main/java/com/example/movie/view/MainActivity.kt
+++ app/src/main/java/com/example/movie/view/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.movie.view
 
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.media.RingtoneManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
 import androidx.fragment.app.Fragment
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import androidx.viewpager.widget.PagerAdapter
 import com.example.movie.R
 import com.example.movie.adapter.SlidePagerAdapter
+import com.example.movie.model.Movie
+import com.example.movie.model.Singleton
 import com.example.movie.myFragments.LikeFragment
 import com.example.movie.myFragments.MainFragment
 import com.example.movie.myFragments.ProfileFragment
@@ -20,10 +31,44 @@ class MainActivity : AppCompatActivity() {
     private var fragmentLike: Fragment = LikeFragment()
     private var fragmentProfile: Fragment = ProfileFragment()
     private var list: MutableList<Fragment> = ArrayList()
+    private var movie: Movie? = Singleton.getMovie()
+
+    private var mRegistrationBroadcastReceiver: BroadcastReceiver? = null
+    override fun onPause() {
+        this.mRegistrationBroadcastReceiver?.let {
+            LocalBroadcastManager.getInstance(this).unregisterReceiver(
+                it
+            )
+        }
+
+        super.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        this.mRegistrationBroadcastReceiver?.let {
+            LocalBroadcastManager.getInstance(this).registerReceiver(
+                it, IntentFilter(
+                    "push"
+                )
+            )
+        }
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_page)
+
+        mRegistrationBroadcastReceiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                if (intent?.action == "push") {
+                    val message = intent.getStringExtra("message")
+                    val title = intent.getStringExtra("title")
+                    showNotification(title, message)
+                }
+            }
+        }
 
         bindView()
         list.add(fragmentMain)
@@ -56,5 +101,37 @@ class MainActivity : AppCompatActivity() {
         pager = findViewById(R.id.pager)
         bottomNavigationView = findViewById(R.id.bottom_navigation)
 
+    }
+
+    private fun showNotification(title: String?, message: String?) {
+        val intent = Intent(this, DetailActivity::class.java)
+        intent.putExtra("movie_id", movie?.id)
+        intent.putExtra("original_title", movie?.original_title)
+        intent.putExtra("movie", movie)
+        intent.putExtra("poster_path", movie?.getPosterPath())
+        intent.putExtra("overview", movie?.overview)
+        intent.putExtra("vote_average", (movie?.vote_average).toString())
+        intent.putExtra("release_date", movie?.release_date)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+
+        val pendingIntent = PendingIntent.getActivity(
+            this, 0, intent,
+            PendingIntent.FLAG_ONE_SHOT
+        )
+
+        val soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder = NotificationCompat.Builder(this)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setWhen(System.currentTimeMillis())
+            .setContentTitle(title)
+            .setContentText(message)
+            .setAutoCancel(true)
+            .setSound(soundUri)
+            .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+            .setContentIntent(pendingIntent)
+
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(0, notificationBuilder.build())
     }
 }
