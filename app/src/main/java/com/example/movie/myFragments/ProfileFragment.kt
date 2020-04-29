ac25eec37d67fb43a468b08f2341460debd1241c
akudaibergen <kudaibergen.adil@gmail.com>
2020-04-29 20:17:04 +0600

    Profile by Kudaibergen

--- app/src/main/java/com/example/movie/myFragments/ProfileFragment.kt
+++ app/src/main/java/com/example/movie/myFragments/ProfileFragment.kt
@@ -1,24 +1,33 @@
 package com.example.movie.myFragments
 
+import android.app.Activity
+import android.app.AlertDialog
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ProgressBar
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.movie.view.LoginActivity
 import com.example.movie.R
+import com.example.movie.api.RequestConstants
 import com.example.movie.model.Singleton
 import com.example.movie.view_model.ProfileViewModel
 import com.example.movie.view_model.ViewModelProviderFactory
+import java.io.File
+import java.io.IOException
 
 class ProfileFragment : Fragment() {
     private lateinit var preferences: SharedPreferences
@@ -28,6 +37,11 @@ class ProfileFragment : Fragment() {
     private lateinit var editor: SharedPreferences.Editor
     private lateinit var progressBar: ProgressBar
     private lateinit var profileListViewModel: ProfileViewModel
+    private lateinit var changeAvatarPhoto: Button
+    private lateinit var avatarIm: ImageView
+    private var photoPath: String? = null
+    private var selectedPhotoFile: File? = null
+    val REQUEST_TAKE_PHOTO=1
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -52,7 +66,6 @@ class ProfileFragment : Fragment() {
                 }
             }
         })
-        preferences = context?.getSharedPreferences("Username", 0) as SharedPreferences
         bindView(rootView)
         return rootView
     }
@@ -63,10 +76,16 @@ class ProfileFragment : Fragment() {
         logout.setOnClickListener {
             profileListViewModel.deleteProfileSession()
         }
+        changeAvatarPhoto.setOnClickListener {
+            getPermissions()
+        }
     }
 
     fun logout() {
-        editor.clear().commit()
+        preferences = context?.getSharedPreferences("Username", 0) as SharedPreferences
+        preferences.edit().clear().commit()
+        preferences = context?.getSharedPreferences("Avatar",0) as SharedPreferences
+        preferences.edit().clear().commit()
         val intent = Intent(activity, LoginActivity::class.java)
         startActivity(intent)
     }
@@ -76,7 +95,8 @@ class ProfileFragment : Fragment() {
         emailInfo = rootView.findViewById(R.id.email)
         logout = rootView.findViewById(R.id.logout)
         progressBar = rootView.findViewById(R.id.progressBar)
-        editor = preferences.edit()
+        avatarIm = rootView.findViewById(R.id.avatarIm)
+        changeAvatarPhoto = rootView.findViewById(R.id.register)
     }
 
     private fun initViews() {
@@ -84,5 +104,133 @@ class ProfileFragment : Fragment() {
         val authorizedEmail = Singleton.getUserName() + "@mail.ru"
         nameInfo.text = authorizedName
         emailInfo.text = authorizedEmail
+        try {
+            preferences = context!!.getSharedPreferences("Avatar",0)
+            val pathPhotoAvatar = preferences.getString("uri",null)
+            avatarIm.setImageURI(Uri.parse(pathPhotoAvatar))
+        }catch (e: Exception){
+            Toast.makeText(activity as Context,"No avatar photos on local",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun getPermissions(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val cameraGranted = ContextCompat.checkSelfPermission(activity as Context,android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
+            val galleryGranted = ContextCompat.checkSelfPermission(activity as Context, android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+            if (cameraGranted && galleryGranted) {
+                imageChooserDialog()
+            } else {
+                requestPermissions(
+                    arrayOf(android.Manifest.permission.CAMERA, android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                    RequestConstants.AVATAR_PERMISSION_REQUEST
+                )
+            }
+        } else {
+            imageChooserDialog()
+        }
+    }
+
+    private fun imageChooserDialog(){
+        val adapter: ArrayAdapter<String> = ArrayAdapter(activity as Context, android.R.layout.simple_list_item_1)
+        adapter.add("Camera")
+        adapter.add("Gallery")
+        AlertDialog.Builder(activity as Context)
+            .setTitle("Change avatar")
+            .setAdapter(adapter) { dialog, which ->
+                if (which == 0) {
+                    openCamera()
+                } else {
+                    openGallery()
+                }
+            }
+            .create()
+            .show()
+    }
+
+    private fun openCamera(){
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if(intent.resolveActivity(context!!.packageManager) !=null){
+            var photoFile: File? = null
+            try{
+                photoFile=createImageFile()
+            }catch (e: IOException){}
+            if(photoFile != null){
+                val photoUri = FileProvider.getUriForFile(
+                    activity as Context,
+                    "${context?.packageName}.provider",
+                    photoFile
+                )
+                intent.putExtra(MediaStore.EXTRA_OUTPUT,photoUri)
+                startActivityForResult(intent,REQUEST_TAKE_PHOTO)
+            }
+        }
+    }
+
+    private fun createImageFile():File?{
+        val filename = "MyAvatars"
+        val storageDir = context?.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        val image = File.createTempFile(
+            filename,
+            ".jpg",
+            storageDir
+        )
+        photoPath = image.absolutePath
+        return image
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        preferences = context!!.getSharedPreferences("Avatar",0)
+        if (resultCode == Activity.RESULT_OK && requestCode==REQUEST_TAKE_PHOTO) {
+            preferences.edit().clear().commit()
+            preferences.edit().putString("uri",photoPath).commit()
+            avatarIm.setImageURI(Uri.parse(photoPath))
+            Toast.makeText(activity as Context,"Your photo is changed",Toast.LENGTH_SHORT).show()
+
+        } else if (requestCode == RequestConstants.GALLERY) {
+            if(data?.data == null){
+                preferences = context!!.getSharedPreferences("Avatar",0)
+                val pathPhotoAvatar = preferences.getString("uri",null)
+                avatarIm.setImageURI(Uri.parse(pathPhotoAvatar))
+                Toast.makeText(activity as Context,"You don't change your photo",Toast.LENGTH_SHORT).show()
+            }
+            else{
+                val image = data?.data
+                preferences.edit().clear().commit()
+                preferences.edit().putString("uri",image.toString()).commit()
+                avatarIm.setImageURI(image)
+                Toast.makeText(activity as Context,"Your photo is changed",Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == RequestConstants.AVATAR_CAMERA_PERMISSION_REQUEST) {
+            if (grantResults.size != 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                openCamera()
+            }
+            return
+        } else if (requestCode == RequestConstants.AVATAR_GALLERY_PERMISSION_REQUEST) {
+            if (grantResults.size != 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                openGallery()
+            }
+            return
+        }
+        else if (requestCode == RequestConstants.AVATAR_PERMISSION_REQUEST) {
+            if (grantResults.size != 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                imageChooserDialog()
+            }
+        }
+    }
+
+    private fun openGallery(){
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true)
+        startActivityForResult(intent, RequestConstants.GALLERY)
     }
 }
